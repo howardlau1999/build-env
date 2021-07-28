6bb296ff2fdbcd99b32e93de2f56e74ae8bded9e
Howard Lau <howardlau1999@hotmail.com>
2021-07-28 13:34:37 +0800

    add buildifier

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,4 +31,17 @@ echo "Installing Bazel..."
 
 curl -fsSL -o /usr/local/bin/bazelisk https://github.com/bazelbuild/bazelisk/releases/download/v1.10.1/bazelisk-linux-amd64
 chmod +x /usr/local/bin/bazelisk
-bazelisk
+bazelisk info
+
+curl -fsSL -o /usr/local/bin/buildifier https://github.com/bazelbuild/buildtools/releases/download/4.0.1/buildifier-linux-amd64
+chmod +x /usr/local/bin/buildifier
+
+INSTALL_DIR="/usr/local/bin"
+VERSION="0.4.5"
+
+# Download and symlink.
+(
+  cd "${INSTALL_DIR}" \
+  && curl -L "https://github.com/grailbio/bazel-compilation-database/archive/${VERSION}.tar.gz" | tar -xz \
+  && ln -f -s "${INSTALL_DIR}/bazel-compilation-database-${VERSION}/generate.sh" bazel-compdb
+)
